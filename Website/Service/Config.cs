43f9207b4b57dd95c0e8b2393872bb87fc32254a
Мария Клепикова <masha.k2001@gmail.com>
2024-05-31 17:13:28 +0300

    [add] панель админа: блок авторизации возможность редактировать страницы

---
+++ Website/Service/Config.cs
@@ -0,0 +1,16 @@
+﻿namespace Website.Service
+{
+	//Создаем спец класс-обертку, св-ва которого соответсвуют св-вам из файла appsettings.json
+	public class Config
+	{
+		public static string ConnectionString { get; set; }
+
+		public static string CompanyName { get; set; }
+
+		public static string CompanyPhone { get; set; }
+
+		public static string CompanyPhoneShort { get; set; }
+
+		public static string CompanyEmail { get; set; }
+	}
+}
